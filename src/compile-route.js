a1bd5f418a0ea3001fec115546f0f0a12ae215c9
Fred Vollmer <fredvollmer@ip-192-168-0-107.us-west-2.compute.internal>
2016-10-07 12:39:06 -0600

    Syntax fix

--- src/compile-route.js
+++ src/compile-route.js
@@ -20,8 +20,8 @@ function getHeaderMatcher (expectedHeaders) {
 
 function normalizeRequest (url, options) {
     if (typeof url === 'string') {
-			concole.log("we've got a string");
-			concole.log("url: " + url + " method: " + options && options.method || 'GET');
+			console.log("we've got a string");
+			console.log("url: " + url + " method: " + options && options.method || 'GET');
         return {
             url: url,
             method: options && options.method || 'GET'
